d5e7e0c9c5c97efade2d98e574b8898381614456
Christophe Avenel <cavenel@gmail.com>
2026-06-21 21:35:58 +0000

    Remove dead get_ip_address/Dropbox block from manage.py

--- manage.py
+++ manage.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
-import socket
-def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
-    
+
 if __name__ == "__main__":
-    #ip = get_ip_address()
-    #with open("/home/cavenel/Dropbox/mahjong_compet/ip.txt",'w') as f:
-    #    f.write(ip)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apps.settings.dev")
     try:
         from django.core.management import execute_from_command_line
